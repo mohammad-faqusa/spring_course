b8c5bc5d40d2e1aa3cb4528865d8aa3e5aa0f449
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-11 10:10:59 +0300

    36. IDE Warning - No Usages

---
+++ section-2-new---spring-core/36-ide-warning---no-usages/note.md
@@ -0,0 +1,8 @@
+## IDE says : `no usages` ??? 
+
+### for example : 
+![img.png](img.png)
+
+## Spring Framework is dynamic 
+IDE may not be able to determine if a given class/method is used at runtime 
+
--- titleConverter.py
+++ titleConverter.py
@@ -47,7 +47,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 2-NEW - Spring Core"
 # section_title = "coding-exercises"
 
-part_title = "35. Constructor Injection - Coding - Part 2"
+part_title = "36. IDE Warning - No Usages"
 print(f"export PART_TITLE='{part_title}'")
 
 prepare_resources(section_title, part_title)
