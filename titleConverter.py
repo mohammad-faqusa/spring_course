1b85a38edeb736cad0e697a713f3b2e9f69148f8
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-14 00:04:35 +0300

    68. JPA Annotations - Overview

--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 3: New - Hibernate/JPA CRUD "
 # section_title = "coding-exercises"
 
-part_title = "67. Setting Up Spring Boot Project - Coding - Part 2"
+part_title = "68. JPA Annotations - Overview"
 print(f"export PART_TITLE='{part_title }'")
 
 prepare_resources(section_title, part_title)
