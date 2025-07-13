9cf85b24a55618a35410ac99d68d23e34e42b111
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-13 11:13:07 +0300

    61. Hibernate, JPA and JDBC

---
+++ section-3-new---hibernate-jpa-crud-/61-hibernate-jpa-and-jdbc/note.md
@@ -0,0 +1,8 @@
+## 61. Hibernate, JPA and JDBC
+
+### How does Hibernate / JPA relate to JDBC ? 
+
+* Hibernate uses JDBC for all database communication 
+* so Hibernate is another abstracttion layer on top of `JDBC`
+
+
--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 3: New - Hibernate/JPA CRUD "
 # section_title = "coding-exercises"
 
-part_title = "60. Hibernate / JPA Overview"
+part_title = "61. Hibernate, JPA and JDBC"
 print(f"export PART_TITLE='{part_title }'")
 
 prepare_resources(section_title, part_title)
