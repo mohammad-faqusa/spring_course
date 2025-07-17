fc2c93b7734f24d9c7be67c06dc4e7403a03753c
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-17 20:09:35 +0300

    137. Spring Boot REST: Spring Data JPA - Coding - Part 2

---
+++ section-4-new---rest-crud-apis/137-spring-boot-rest-spring-data-jpa---coding---part-2/note.md
@@ -0,0 +1,8 @@
+## 137. Spring Boot REST: Spring Data JPA - Coding - Part 2
+
+### development process
+1. check the `/employees` GET 
+2. check to get `/employee/{employeeId}`
+3. add new employee POST `/employees`
+4. PUT request `employees`
+5. PATCH `/employee/{employeeId}`
--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 4: New - REST CRUD APIs"
 # section_title = "coding-exercises"
 
-part_title = "136. Spring Boot REST: Spring Data JPA - Coding - Part 1"
+part_title = "137. Spring Boot REST: Spring Data JPA - Coding - Part 2"
 print(f"export PART_TITLE='{part_title }'")
 
 prepare_resources(section_title, part_title)
