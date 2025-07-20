e7aab31d8374526533708d16dd473cd45107fa5a
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-20 05:48:28 +0300

    162. Spring Boot REST API Security - JDBC Authentication - Plain Text - Coding Part 1

--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 5: New - REST API Security"
 # section_title = "coding-exercises"
 
-part_title = "161. Spring Boot REST API Security - JDBC Authentication - Plain Text - Overview"
+part_title = "162. Spring Boot REST API Security - JDBC Authentication - Plain Text - Coding Part 1"
 print(f"export PART_TITLE='{part_title }'")
 
 prepare_resources(section_title, part_title)
