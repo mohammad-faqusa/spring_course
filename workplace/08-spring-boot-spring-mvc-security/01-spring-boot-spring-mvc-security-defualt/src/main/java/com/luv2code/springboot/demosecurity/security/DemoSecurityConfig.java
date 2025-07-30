9e726eafccf7dd299f4e0092b4240550416d3e5e
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-30 19:59:49 +0300

    250. Spring MVC Security - Custom Login Form - Coding - Part 1

--- workplace/08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-defualt/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ workplace/08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-defualt/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.luv2code.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -32,5 +34,12 @@ public class DemoSecurityConfig {
 
         return  new InMemoryUserDetailsManager(john, mary, suzan);
 
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()).formLogin(form -> form.loginPage("/showLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll());
+        return http.build();
     }
 }
