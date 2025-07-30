abe172c6957110e518887e65f512202199f068c6
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-30 19:32:48 +0300

    247. Spring MVC Security - Basic Configuration - Coding""248. Spring MVC Security - Custom Login Form - Overview - Part 1"

--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 8: New - Spring MVC Security"
 # section_title = "coding-exercises"
 
-part_title = "247. Spring MVC Security - Basic Configuration - Coding"
+part_title = "248. Spring MVC Security - Custom Login Form - Overview - Part 1"
 print(f"export PART_TITLE='{part_title }'")
 
 prepare_resources(section_title, part_title)
