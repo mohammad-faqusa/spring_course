22d190e0c91aafe2f2f04f0e279c5fc7e1469ea8
Mohammad Faqusa <201014@ppu.edu.ps>
2025-07-30 14:01:21 +0300

    238. CRUD Database Project - Update Employee - Coding

---
+++ section-7-new---spring-mvc-crud/238-crud-database-project---update-employee---coding/note.md
@@ -0,0 +1,9 @@
+## 238. CRUD Database Project - Update Employee - Coding
+
+#### Step 1 : "Update" button to page 
+
+1. go to employee list form 
+2. add the column of update 
+
+`employees/showFromForUpdate(employeeId=${tempEmployee.id})`
+
--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 7: New - Spring MVC CRUD"
 # section_title = "coding-exercises"
 
-part_title = "237. CRUD Database Project - Update Employee - Overview"
+part_title = "238. CRUD Database Project - Update Employee - Coding"
 print(f"export PART_TITLE='{part_title }'")
 
 prepare_resources(section_title, part_title)
