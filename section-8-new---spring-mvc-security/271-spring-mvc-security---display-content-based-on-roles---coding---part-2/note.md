4ad3937b525cb29570bfe0fc1105d55296bb31eb
Mohammad Faqusa <201014@ppu.edu.ps>
2025-08-02 12:06:04 +0300

    271. Spring MVC Security - Display Content Based on Roles - Coding - Part 2

---
+++ section-8-new---spring-mvc-security/271-spring-mvc-security---display-content-based-on-roles---coding---part-2/note.md
@@ -0,0 +1,5 @@
+## 271. Spring MVC Security - Display Content Based on Roles - Coding - Part 2
+
+### The process 
+1. try to login with different roles and see what pages are displayed in home page 
+2. 
--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 8: New - Spring MVC Security"
 # section_title = "coding-exercises"
 
-part_title = "270. Spring MVC Security - Display Content Based on Roles - Coding - Part 1"
+part_title = "271. Spring MVC Security - Display Content Based on Roles - Coding - Part 2"
 print(f"export PART_TITLE='{part_title }'")
 
 prepare_resources(section_title, part_title)
