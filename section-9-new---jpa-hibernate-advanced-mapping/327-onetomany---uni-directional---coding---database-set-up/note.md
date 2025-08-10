53909c2bbda066a5c3f39bdbee8669f732360e76
mohammadfaqusa <201014@ppu.edu.ps>
2025-08-10 13:02:07 +0300

    327. @OneToMany - Uni-Directional - Coding - Database Set Up

---
+++ section-9-new---jpa-hibernate-advanced-mapping/327-onetomany---uni-directional---coding---database-set-up/note.md
@@ -0,0 +1,11 @@
+## 327. @OneToMany - Uni-Directional - Coding - Database Set Up
+
+### Development Process 
+1. Prep work - define database tables 
+2. Create Review class 
+3. Update Course class 
+
+#### Step 1. Prep work - define database tables 
+1. move to dev directory , `09`, `hb-04`
+2. run the sql script 
+3. 
--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 9: New - JPA / Hibernate Advanced Mapping"
 # section_title = "coding-exercises"
 
-part_title = "326. @OneToMany - Uni-Directional - Overview"
+part_title = "327. @OneToMany - Uni-Directional - Coding - Database Set Up"
 print(f"export PART_TITLE='{part_title }'")
 
 prepare_resources(section_title, part_title)
