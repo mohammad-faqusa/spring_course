894159446e76c50dcfd21b2dfead0299a443c29e
Mohammad Faqusa <103820177+mohammad-faqusa@users.noreply.github.com>
2025-08-15 06:45:47 +0300

    396. AOP: @Around Advice - Coding - Part 3

---
+++ section-10-new---aop-aspect-oriented-programming/396-aop-around-advice---coding---part-3/note.md
@@ -0,0 +1,6 @@
+## 396. AOP: @Around Advice - Coding - Part 3
+
+### The process
+1. copy the code of printing the method name, .getSignature, the start time, end time, duraiton
+2. run the application 
+3. 
--- titleConverter.py
+++ titleConverter.py
@@ -58,7 +58,7 @@ def create_code_exercise(section_title, part_title):
 section_title = "Section 10: New - AOP: Aspect-Oriented Programming"
 # section_title = "coding-exercises"
 
-part_title = "395. AOP: @Around Advice - Coding - Part 2"
+part_title = "396. AOP: @Around Advice - Coding - Part 3"
 print(f"export PART_TITLE='{part_title }'")
 
 prepare_resources(section_title, part_title)
